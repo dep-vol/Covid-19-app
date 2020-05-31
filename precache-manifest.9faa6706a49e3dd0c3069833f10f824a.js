cdab8235baf568a10226f08bcbeccd2c7d89e2ec
dep-vol <alex.tecto.msk@gmail.com>
2020-05-31 06:48:18 +0300

    Updates

---
+++ precache-manifest.9faa6706a49e3dd0c3069833f10f824a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e6123f249344903d32878498225eb2",
+    "url": "/Covid-19-app/index.html"
+  },
+  {
+    "revision": "aee596bc96249ba33cef",
+    "url": "/Covid-19-app/static/css/main.cd7410ec.chunk.css"
+  },
+  {
+    "revision": "61dd3463e8f22b4b0b5b",
+    "url": "/Covid-19-app/static/js/2.8e7e0481.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/Covid-19-app/static/js/2.8e7e0481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aee596bc96249ba33cef",
+    "url": "/Covid-19-app/static/js/main.f92252bf.chunk.js"
+  },
+  {
+    "revision": "f2e2fe1acafd34118a41",
+    "url": "/Covid-19-app/static/js/runtime-main.8f3a1105.js"
+  },
+  {
+    "revision": "172ef0265cb69e1e84475c82d9320525",
+    "url": "/Covid-19-app/static/media/virus_logo.172ef026.png"
+  }
+]);
